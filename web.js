3d57a840caa78d0e5c74ec816359efdba6342c20
gillete98 <julian.loaiza@gmail.com>
2013-07-17 04:49:14 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+
+var fs = require ('fs'); 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.readFile(path.join(process.cwd(), 'index.html'), function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(datax);
 });
 
 var port = process.env.PORT || 5000;
