70103b6c2a030a7fb14bd493e11deaed980edfe9
gillete98 <julian.loaiza@gmail.com>
2013-07-19 01:50:34 +0000

    Web 5

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 
-//var fs = require ('fs'); 
+var fs = require ('fs'); 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//fs.readFile('index.html', function (err, data) {
-//  if (err) throw err;
-//  console.log(data);
-//});
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 
 app.get('/', function(request, response) {
   response.send("text");
